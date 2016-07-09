a79bf27ed45754f83a57ada48f102024ab16a186
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2016-07-09 16:00:59 +0200

    Integrate start page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,6 @@
 class StaticController < ApplicationController
   def start
+    @pages = Page.order("updated_at DESC").limit(3)
+    @posts = GuestbookPost.order("updated_at DESC").limit(4)
   end
 end
