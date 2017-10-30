49a8b4963b193d8436cee289e053b46841dfc5ca
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2017-10-30 15:42:52 +0100

    Fix broken migration

--- db/migrate/20160329115133_create_guestbook_posts.rb
+++ db/migrate/20160329115133_create_guestbook_posts.rb
@@ -1,6 +1,6 @@
 class CreateGuestbookPosts < ActiveRecord::Migration[5.0]
   def change
-    create_table :guestbook do |t|
+    create_table :guestbook_posts do |t|
       t.string :title
       t.text :body
       t.references :user, foreign_key: true
